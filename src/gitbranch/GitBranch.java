f66884b38476f0fe6cb71c303de8ea511ec5a4a3
Tar Gergő <tar.gergo21@gmail.com>
2024-09-30 13:28:28 +0200

    fuggveny static implamentalas

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -7,7 +7,7 @@ public class GitBranch {
     }
 
     private static int fv() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        System.out.println("static");
+    return -1;}
     
 }
